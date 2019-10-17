e9d9a7d06090a72b530ae5bc471f91083b5d03e1
Stuart <stuart@miller-media.com>
2019-10-17 16:12:55 -0700

    MM-63 Refactor plugin to use classes and sanatize $_POST input.

--- inc/edit-usernames.php
+++ inc/edit-usernames.php
@@ -1,66 +1,71 @@
 <?php
-/**
-Plugin Name: Edit Usernames
-Description: Change a user's username within the admin screen.
-Author: Miller Media ( Michael Robinson )
-Author URI: http://www.millermedia.io
-Version: 1.0
-Requires PHP: 5.6
-License: GPLv2
-Text Domain: edit-usernames
-*/
 
-include( 'inc/helpers.php' );
+if ( ! defined( 'ABSPATH' ) ) {
+    die( 'Access denied.' );
+}
 
-if ( is_admin() ){
+class EUN_EditUsernames {
 
-    function username_admin_notice__error() {
+    /**
+     * Add hooks
+     */
+    public function __construct()
+    {
+        if ( isset( $_GET['usernameexists'] ) && $_GET['usernameexists'] == 'yes' ) {
+            add_action( 'admin_notices', array( $this, 'username_admin_notice__error' ) );
+        }
+
+        add_action( 'edit_user_profile_update', array( $this, 'update_username' ), 0 );
+        add_action( 'edit_user_profile', array( $this, 'update_user_nicename' ) );
+        add_action( 'edit_user_profile', array( $this, 'update_comment_author' ) );
+
+        if ( EUN_SELFEDIT ) {
+            add_action( 'personal_options_update', array( $this, 'update_username' ), 0 );
+            add_action( 'show_user_profile', array( $this, 'update_user_nicename' ) );
+            add_action( 'show_user_profile', array( $this, 'update_comment_author' ) );
+        }
+    }
+
+    /**
+     * Display WordPress error message if username already exists
+     */
+    public function username_admin_notice__error()
+    {
         $class = 'notice notice-error';
         $message = __( 'ERROR: Username already exists. Please choose another.', 'edit-usernames' );
 
         printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $message );
     }
 
-    // Display WordPress error message if username already exists
-    if ( isset($_GET['exists'])){
-        if ( $_GET['exists'] == 'yes') {
-            add_action( 'admin_notices', 'username_admin_notice__error' );
-        }
-    }
-
-    function update_username($user_id){
-
+    /**
+     * Check the new username and update if appropriate
+     *
+     * @param  int   $user_id
+     */
+    public function update_username( $user_id )
+    {
         // Only allow admins to edit usernames
-        /*
-         * 10/27/16
-         * Added support for WooCommerce Managers
-         */
-        if( (current_user_can('administrator') || current_user_can( 'manage_woocommerce' ) || current_user_can('edit_users')) && !(is_super_admin($user_id))){
+        if ( ( current_user_can( 'administrator' ) || current_user_can( 'manage_woocommerce' ) || current_user_can('edit_users') ) && !( is_super_admin ( $user_id ) ) ) {
 
             global $wpdb;
 
             // If someone has submitted a new user login
-            if ( isset($_POST['user_login']) && !empty($_POST['user_login'])){
+            if ( isset( $_POST['user_login'] ) && ! empty( $_POST['user_login'] ) ) {
 
-                $current_username = get_user_by('id', $user_id)->user_login;
-                $current_username = esc_sql($current_username);
-//                $new_username = sanitize_user($_POST['user_login']);
-                $new_username = $_POST['user_login'];
-                $new_username = esc_sql($new_username);
+                $current_username = get_user_by( 'id', $user_id )->user_login;
+                $current_username = esc_sql( $current_username );
+                $new_username = sanitize_text_field( $_POST['user_login'] );
+                $new_username = esc_sql( $new_username );
 
                 // Nothing has changed, so we exit
                 if ( $current_username == $new_username ) {
                     return;
-                }
-
-                elseif ( username_exists($new_username)){
+                } elseif ( username_exists( $new_username ) ) {
                     $return_url = wp_get_referer();
-                    $redirect = add_query_arg( 'exists', 'yes', $return_url );
+                    $redirect = add_query_arg( 'usernameexists', 'yes', $return_url );
                     wp_safe_redirect( $redirect );
                     exit();
-                }
-
-                else {
+                } else {
                     // Update username and related values
                     $sql_userlogin = $wpdb->prepare( "UPDATE $wpdb->users SET user_login = %s WHERE user_login = %s", $new_username, $current_username );
 
@@ -71,7 +76,7 @@ if ( is_admin() ){
 
                     // Is nickname the same as username?
                     $sql_usermeta = $wpdb->prepare( "SELECT meta_value from $wpdb->usermeta WHERE user_id = %s AND meta_key = 'nickname'", $user_id );
-                    $usermeta_info = $wpdb->get_row( $sql_usermeta);
+                    $usermeta_info = $wpdb->get_row( $sql_usermeta );
                     $nickname = $usermeta_info->meta_value;
 
                     // If display name == username, update both
@@ -104,11 +109,11 @@ if ( is_admin() ){
             }
         }
     }
-    add_action( 'edit_user_profile_update', 'update_username', 0 );
-//    add_action( 'personal_options_update', 'update_username', 0 ); // uncomment to allow self-editing
+
 
     // Check whether nicename is sanitized version of login; update if not
-    function update_user_nicename($user_id){
+    public function update_user_nicename( $user_id )
+    {
         global $wpdb;
 
         $user_login = $user_id->user_login;
@@ -116,25 +121,19 @@ if ( is_admin() ){
 
         if ( $user_nicename != strtolower( str_replace( ' ', '-', $user_login ) )){
             $sql_nicename = $wpdb->prepare( "UPDATE $wpdb->users SET user_nicename = %s WHERE user_login = %s", strtolower( str_replace( ' ', '-', $user_login ) ), $user_login );
-            $wpdb->query($sql_nicename);
+            $wpdb->query( $sql_nicename );
         }
-
     }
+
     // Update comment author
-    function update_comment_author($user_id){
+    public function update_comment_author( $user_id )
+    {
         global $wpdb;
 
         $id = $user_id->ID;
         $display_name = $user_id->display_name;
 
-        $sql_comment_author = $wpdb->prepare("UPDATE $wpdb->comments SET comment_author = %s WHERE user_id = %s", $display_name, $id);
+        $sql_comment_author = $wpdb->prepare( "UPDATE $wpdb->comments SET comment_author = %s WHERE user_id = %s", $display_name, $id );
         $wpdb->query( $sql_comment_author );
     }
-    add_action('edit_user_profile', 'update_user_nicename');
-//    add_action('show_user_profile', 'update_user_nicename'); // uncomment to allow self-editing
-    add_action('edit_user_profile', 'update_comment_author');
-//    add_action('show_user_profile', 'update_user_nicename'); // uncomment to allow self-editing
 }
-
-
-
---
+++ plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+Plugin Name: Edit Usernames
+Description: Change a user's username within the admin screen.
+Author: Miller Media ( Michael Robinson )
+Author URI: http://www.millermedia.io
+Version: 1.0
+Requires PHP: 5.6
+License: GPLv2
+Text Domain: edit-usernames
+*/
+
+if ( is_admin() ){
+
+    // boolean to control whether to allow self editing
+    define( 'EUN_SELFEDIT', false );
+    define( 'EUN_PLUGIN_PATH', plugin_dir_path(__FILE__) );
+
+    require_once( EUN_PLUGIN_PATH . 'inc/helpers.php' );
+    require_once( EUN_PLUGIN_PATH . 'inc/edit-usernames.php' );
+
+    new EUN_Helpers();
+    new EUN_EditUsernames();    
+}
